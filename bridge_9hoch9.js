7ba3c6fc3b50f185b5777a86732188e4b6838cec
iki1uc <entertrainer@web.de>
2026-07-10 00:06:58 +0200

    Implement readCube function in bridge_9hoch9.js
    Add readCube function to process cube data and update core temporary state.

--- bridge_9hoch9.js
+++ bridge_9hoch9.js
@@ -1,1 +1,18 @@
+// META-CORE / bridge_9hoch9.js
+
+import { api9hoch9 } from "../9hoch9/api.js";
+import { updateCoreTmp } from "./tmp.js";
+
+export function readCube(Phi, phi, phi2, phiinfty) {
+    const cube = api9hoch9(Phi, phi, phi2, phiinfty);
+
+    updateCoreTmp({
+        cube: cube,
+        axes: cube.axes,
+        lage: cube.lage,
+        ghost: cube.ghost
+    });
+
+    return cube;
+}
 
