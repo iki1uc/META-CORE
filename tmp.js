c19a87043e7971807e88a5bed1304562f93009ab
iki1uc <entertrainer@web.de>
2026-07-10 00:06:42 +0200

    Update tmp.js

--- tmp.js
+++ tmp.js
@@ -1,1 +1,18 @@
+// META-CORE / tmp.js
+
+export const tmpCore = {
+    lastCube: null,
+    lastMatrix: null,
+    lastAxes: null,
+    lastLAGE: null,
+    lastGhost: null
+};
+
+export function updateCoreTmp(data) {
+    tmpCore.lastCube = data.cube || null;
+    tmpCore.lastMatrix = data.matrix || null;
+    tmpCore.lastAxes = data.axes || null;
+    tmpCore.lastLAGE = data.lage || null;
+    tmpCore.lastGhost = data.ghost || null;
+}
 
