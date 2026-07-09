fd406acc1c05378e693d4da1aca12ce481d0d4d3
iki1uc <entertrainer@web.de>
2026-07-10 00:08:42 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,8 @@
+// META-CORE / api.js
+
+import { interpretSystem } from "./core.js";
+
+export function metaCore(Phi, phi, phi2, phiinfty) {
+    return interpretSystem(Phi, phi, phi2, phiinfty);
+}
 
