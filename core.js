1c5c776e2ced94c1515e4fc330a4e45f7bf782ac
iki1uc <entertrainer@web.de>
2026-07-10 00:07:25 +0200

    Update core.js

--- core.js
+++ core.js
@@ -1,1 +1,18 @@
+// META-CORE / core.js
+
+import { readCube } from "./bridge_9hoch9.js";
+import { readMatrix } from "./bridge_81.js";
+
+export function interpretSystem(Phi, phi, phi2, phiinfty) {
+
+    const cube = readCube(Phi, phi, phi2, phiinfty);
+    const matrix = readMatrix();
+
+    return {
+        core: "META-CORE",
+        cube,
+        matrix,
+        status: "ready"
+    };
+}
 
