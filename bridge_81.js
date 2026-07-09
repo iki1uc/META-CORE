98a08c1039d98baa362033e630d3c2fcf9eb6e24
iki1uc <entertrainer@web.de>
2026-07-10 00:07:12 +0200

    Implement readMatrix function in bridge_81.js
    Added readMatrix function to retrieve and update matrix data.

--- bridge_81.js
+++ bridge_81.js
@@ -1,1 +1,15 @@
+// META-CORE / bridge_81.js
+
+import { getCube, getAllCubes } from "../81_81_1/api.js";
+import { updateCoreTmp } from "./tmp.js";
+
+export function readMatrix() {
+    const matrix = getAllCubes();
+
+    updateCoreTmp({
+        matrix: matrix
+    });
+
+    return matrix;
+}
 
